78383daa88de000e777dbba3af168fa1d5f43e45
Blog 2.0 <eunv2004@gmail.com>
2020-09-20 14:26:10 +0300

    Select even

--- DataBase.cs
+++ DataBase.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace MCOT_prj
 {
-    class DataBase
+    class DataBase:Form
     {
         public static string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"Main.accdb\"";
 
@@ -16,12 +16,12 @@ namespace MCOT_prj
 
         public OleDbConnection con = new OleDbConnection(connectionString);
         string query;
-
+        public string activeGroup { get; set; }
         public List<string> groups = new List<string>();
 
         public void findGroups()
         {
-            Form1 f = new Form1();
+
 
             query = "SELECT MainTable.group FROM MainTable";
             OleDbCommand command = new OleDbCommand(query, con);
@@ -37,6 +37,24 @@ namespace MCOT_prj
             }
             reader.Close();
 
+        }
+
+
+        public List<string> GetSubj( string day)
+        {
+
+            List<string> subjects = new List<string>();
+            query = "SELECT even FROM MainTable WHERE MainTable.group='"+activeGroup+"' AND MainTable.dayoftheweek ='"+day+"'";
+            MessageBox.Show(query);
+            OleDbCommand command = new OleDbCommand(query, con);
+            OleDbDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                subjects.Add(reader.GetString(0));
+            }
+
+            return subjects;
         }
 
     }
